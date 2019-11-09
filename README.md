3e9df672fffe58217ca5cbe1de5314c417d27493
Arian Stolwijk <stolwijk.arian@gmail.com>
2019-11-09 11:55:28 +0100

    Add some more README and gradle/maven publising stuff

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
 Kotlin GraphQL Test DSL for graphql-java
 ========================================
 
-Work in Progress
+This is a kotlin DSL to easily write (integration) tests for your [graphql-java](https://github.com/graphql-java/graphql-java)
+application.
+
+It is inspired by the [Spring MockMVC DSL](https://docs.spring.io/spring/docs/current/spring-framework-reference/languages.html#mockmvc-dsl)
+and lets you use [JsonPath](https://github.com/json-path/JsonPath) to quickly retrieve results from the response.
+
+![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.symbaloo/graphql-kotlin-test-dsl/badge.svg)
+![Actions](https://github.com/arian/graphql-kotlin-test-dsl/workflows/CI/badge.svg)
+
+### Gradle
+
+```kotlin
+testImplementation("com.symbaloo:grapql-kotlin-test-dsl:1.0.1")
+```
+
+### Maven
+
+```xml
+<dependency>
+  <groupId>com.symbaloo</groupId>
+  <artifactId>graphql-kotlin-test-dsl</artifactId>
+  <version>1.0.1</version>
+</dependency>
+```
+
+Example
+-------
+
+The following code shows an example how to use this test library.
+
+```kotlin
+// the graphql-java schema
+val schema: GraphQL = createTestSchema()
+graphQLTest(schema) {
+    // define a query
+    query("""
+        |query Init(${"$"}echo: String) {
+        |    echo(echo: ${"$"}echo)
+        |    hello { hello }
+        |}""".trimMargin())
+    // add a variable
+    variable("echo", "response")
+}.andExpect {
+    // do assertions
+    noErrors()
+    pathIsEqualTo("echo", "response")
+    withPath<String?>("\$.hello.hello") {
+        assertThat(it).isEqualTo("world")
+    }
+    withPath<Map<String, Any>>("\$.hello") {
+        assertThat(it).contains("hello", "world")
+    }
+}
+```
+
+License
+-------
+
+MIT License
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
     kotlin("jvm") version "1.3.50"
     id("org.jlleitschuh.gradle.ktlint") version "9.0.0"
+    `java-library`
+    `maven-publish`
+    signing
 }
+
+group = "com.symbaloo"
+version = "1.0.1-SNAPSHOT"
+description = "A Kotlin DSL to write Tests for graphql-java"
+val repoDescription = description
+val repoUrl = "https://github.com/arian/graphql-kotlin-test-dsl"
 
 repositories {
     mavenCentral()
@@ -24,4 +33,75 @@ tasks.test {
     testLogging {
         events("passed", "skipped", "failed")
     }
+}
+
+tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+
+            pom {
+                name.set("GraphQL Kotlin Test DLS")
+                description.set(repoDescription)
+                url.set(repoUrl)
+
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("http://www.opensource.org/licenses/mit-license.php")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("arian")
+                        name.set("Arian Stolwijk")
+                        email.set("arian@symbaloo.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/arian/graphql-kotlin-test-dsl.git")
+                    url.set("https://github.com/arian/graphql-kotlin-test-dsl")
+                }
+            }
+        }
+
+        repositories {
+            maven {
+
+                val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+                val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
+                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+                // these can be set through gradle.properties
+                if (properties.containsKey("mavenRepoUser")) {
+                    credentials {
+                        username = properties["mavenRepoUser"] as String
+                        password = properties["mavenRepoPassword"] as String
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }

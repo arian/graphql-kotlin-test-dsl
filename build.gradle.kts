cb232b94e49c9e5250128246a5bfff05bf6e6e0a
Arian Stolwijk <stolwijk.arian@gmail.com>
2019-11-09 13:26:17 +0100

    Version 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.symbaloo"
-version = "1.0.1-SNAPSHOT"
+version = "1.0.1"
 description = "A Kotlin DSL to write Tests for graphql-java"
 val repoDescription = description
 val repoUrl = "https://github.com/arian/graphql-kotlin-test-dsl"

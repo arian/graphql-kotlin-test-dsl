97fb1090b6c26df7de36107729081bb8172914b4
Arian Stolwijk <stolwijk.arian@gmail.com>
2019-12-06 11:51:14 +0100

    Only add slf4j-simple as test dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ dependencies {
     implementation("com.jayway.jsonpath:json-path:2.4.+")
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("org.opentest4j:opentest4j:1.2.0")
-    implementation("org.slf4j:slf4j-simple:1.7.+")
     implementation("org.slf4j:slf4j-api:1.7.+")
+    testImplementation("org.slf4j:slf4j-simple:1.7.+")
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.20")
 }
